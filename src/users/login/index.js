445cd584b22c6afbb5d55d0f8262fa540b56ce27
dkippes <kippes.diego@gmail.com>
2021-03-10 20:34:52 -0300

    login

--- src/users/login/index.js
+++ src/users/login/index.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
 const codeResponse = require("../../utils/statusCode");
+const userSchema = require("../../utils/userSchema");
 
 AWS.config.update({
   region: "us-east-2",
@@ -7,21 +8,34 @@ AWS.config.update({
 });
 
 async function loginUser(event, context) {
+  const DocumentClient = new AWS.DynamoDB.DocumentClient();
 
-    console.log(event.body);
+  try {
+    //Parsea la data
+    const data =
+      typeof event.body === "string" ? JSON.parse(event.body) : event.body;
 
-    await DocumentClient.query({
-        TableName: "users",
-        Item,
-      }).promise();
+    // Se fija si el email se encuentra en la base de datos y la pass coincide
+    const isUserRegister = await DocumentClient.scan({
+      TableName: "users",
+      ExpressionAttributeValues: {
+        ":e": data.email,
+        ":p": data.password
+      },
+      FilterExpression: "email = :e AND password = :p ",
+      ProjectionExpression: "email",
+    }).promise();
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            messages: 'login'
-        })
-    };
-
+    //Si encuentro al usuario
+    if (isUserRegister.Items[0] == undefined) {
+      return codeResponse(210, "Usuario no registrado / Error de credenciales");
+    } else {     
+      return codeResponse(200, "Usuario logueado con exito");
+    }
+    
+  } catch {
+    return codeResponse(500, "Hubo un problema en el servidor");
+  }
 }
 
 module.exports = { loginUser };
